cd01d937cb82b98ced26fe8cfcd77aa328961b8f
Felix Wagner <post@felixwagner.eu>
2025-02-07 11:04:44 +0100

    first commit: base setup

---
+++ scripts/script.js
@@ -0,0 +1,21 @@
+// Funktionalitäten
+// Gerichte hinzufügen durch klicken auf Plus Symbol am rechten oberen Rand
+// Gerichte hinzufügen, wenn sie im Warenkorb sind
+// Gerichtemenge reduzieren, wenn sie im Warenkorb sind
+// Mengengerechte Preisberechnung
+// Gericht löschen im Warenkorb
+// Berechnung Zwischensumme
+// Berechnung Lieferkosten
+// Berechnung Gesamtsumme
+// Schalter mit Lieferung oder Selbstabholung
+// Bestellen-Button: Meldung, dass Bestellung vorgenommen wurde; kein Alert!. Leerung des Warenkorbs.
+// Auch wenn alle Gerichte ausgewählt werde, darf der Warenkorb nicht "überlaufen"
+// Der Desktop-Warenkorb sollte immer oben an der Seite angeheftet sein, außer wenn man ganz oben oder zanb unten auf der Seite ist.
+//
+// Code
+// Aussagekräftige Namen für Fkt und Variablen
+// camelCase
+// Formatierung
+// Höchstens 14 Zeilen pro Fkt.
+// Gleicher Abstand zwischen Fkt (1 oder 2 Leerzeichen).
+// Lange HTML-templates ind extra Funktionen
